a9c5806e548e6c7ae9d50c7efd9283a98de5a633
Yunior Mesa <yuniorm001@gmail.com>
2019-11-06 19:06:48 -0400

    pagina web practica III

---
+++ pagina web practica III/pagina web practica III/Controllers/InicioController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pagina_web_practica_III.Controllers
+{
+    public class InicioController : Controller
+    {
+        // GET: Inicio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Nosotros()
+        {
+            return View();
+        }
+
+        public ActionResult Galeria()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Contactos()
+        {
+            return View();
+        }
+    }
+}
---
+++ pagina web practica III/pagina web practica III/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pagina_web_practica_III.MvcApplication" Language="C#" %>
